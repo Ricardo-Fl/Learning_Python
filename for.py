7fcacc8fd9e8626d493f5e4971dc31742e409ad9
RicardoFH <rflores_h@outlook.com>
2020-10-27 12:42:02 -0600

    For

---
+++ for.py
@@ -0,0 +1,23 @@
+"""Clase de For"""
+"""
+names = ['Abraham', 'Daniel', 'Cesar', 'Daniela', 'Diego', 'Edgar']
+
+for name in names:
+    print(f'Student: {name}')
+else:
+    print('No more names')
+"""
+"""
+string = 'Ricardo'
+
+for char in string:
+    if char != 'i':
+        print(char)
+"""
+
+numbers = []
+
+for number in range(0, 21, 2):
+    numbers.append(number)
+
+print(numbers)
